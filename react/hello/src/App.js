3802ac20c916e119b0b596ac93bec9e6e4b3a6ba
bradguru2 <bradley.dingess@comcast.net>
2018-05-19 01:49:53 -0700

    Added Unbeatable AI -- minimax

--- react/hello/src/App.js
+++ react/hello/src/App.js
@@ -66,6 +66,16 @@ function calculateWinner(squares) {
   return null;
 }
 
+function isMoreMoves(squares){
+  for(var value of squares){
+    if(value === null){
+      return true;
+    }
+  }
+
+  return false;
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -78,16 +88,82 @@ class App extends React.Component {
     };
   }
 
-  handleClick(i) {
+  getAvailableMoves(squares){
+    let moves = [];
+
+    for(let i=0; i < squares.length; i++){
+      if(squares[i] === null){
+        moves.push({ index: i, score: null });
+      }
+    }
+    
+    return moves;
+  }
+
+  miniMax(squares, player){
+    let win = calculateWinner(squares);
+    let cmpScore = player === 'O' ? -20 : 20;
+    let spots = this.getAvailableMoves(squares);
+
+    if(win != null){
+      if(win === 'O'){
+        return {score:10};
+      }
+      else {
+        return {score:-10};
+      }
+    }
+    else if(spots.length === 0){
+      return {score:0};
+    }
+
+    let moves = [];
+    for (var spot of spots) {
+      squares[spot.index] = player;
+      spot.score = this.miniMax(squares, player === 'O' ? 'X' : 'O').score;
+      squares[spot.index] = null;
+     
+      if ((player === 'X' && spot.score === -10) || (player === 'O' && spot.score === 10)) {
+        return spot;
+      }
+      else{
+        moves.push(spot);
+      }
+    }
+
+    let bestMove = null;
+    for(var move of moves){
+      if(player === 'O' && move.score > cmpScore){
+        bestMove = move;
+        cmpScore = move.score;
+      }
+      else if(player === 'X' && move.score < cmpScore){
+        bestMove = move;
+        cmpScore = move.score;
+      }
+    }
+
+    return bestMove;
+  }
+
+  bestSpot(squares){
+    let spot = this.miniMax(squares, 'O');
+    //alert('(' + spot.score + ',' + spot.index + ')');
+    return spot.index;
+  }
+
+  computerTurn(){
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    
-    if (calculateWinner(squares) || squares[i]) {
+
+    let moves = this.getAvailableMoves(squares);
+
+    if(moves.length === 0 || calculateWinner(squares)){
       return;
     }
 
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    squares[this.bestSpot(squares)] = 'O';
 
     this.setState({
       history: history.concat([{
@@ -98,11 +174,36 @@ class App extends React.Component {
     });
   }
 
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    
+    if (!this.state.xIsNext || calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+    setTimeout(function lambda(){this.computerTurn()}.bind(this), 50);
+
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
   jumpTo(step) {
     this.setState({
       stepNumber: step,
       xIsNext: (step % 2) === 0,
     });
+
+    if(step % 2 === 1) 
+      setTimeout(function lambda(){this.computerTurn()}.bind(this), 50);
   }
 
   render() {
@@ -122,8 +223,12 @@ class App extends React.Component {
     });
 
     let status;
+    let tie = !isMoreMoves(current.squares);
+
     if (winner) {
       status = 'Winner: ' + winner;
+    } else if(tie){
+      status = 'Tie';
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
